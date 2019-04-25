99543545748766dff290e232c2ed4504ebd17e27
MrPauL91 <jupvillegasgo@unal.edu.co>
2019-04-25 18:23:19 -0500

    added advance for data way form

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-data',
@@ -7,9 +8,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataComponent implements OnInit {
 
-  constructor() { }
+  forma: FormGroup;
 
-  ngOnInit() {
+
+  usuario: Usuario = {
+    nombrecompleto: {
+      nombre: 'Pedro',
+      apellido: 'Isaza'
+    },
+    correo: 'pisaza@unal.edu.co'
   }
 
+  constructor() {
+
+    this.forma = new FormGroup({
+      'nombrecompleto': new FormGroup({
+        'nombre': new FormControl('', [Validators.required,
+                                       Validators.minLength(3)]),
+        'apellido': new FormControl('', Validators.required)
+      }),
+      'correo': new FormControl('', [Validators.required,
+      Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]
+      )
+    });
+
+    //Carga objeto por defecto.
+    this.forma.setValue(this.usuario);
+  }
+
+  ngOnInit() {
+
+
+  }
+
+  guardarCambios() {
+
+    console.log(this.forma.value);
+    console.log(this.forma);
+
+    this.forma.reset({
+      nombrecompleto: {
+        nombre: "",
+        apellido: ""
+      },
+      correo: ""
+    });
+  }
+
+}
+
+
+
+export interface Usuario {
+  nombrecompleto: {
+    apellido: string;
+    nombre: string;
+  }
+  correo: string;
 }
