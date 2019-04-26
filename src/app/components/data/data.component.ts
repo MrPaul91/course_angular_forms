b32223abfd3f19838d76cfb9a9a7c70ee83b60aa
MrPauL91 <jupvillegasgo@unal.edu.co>
2019-04-26 15:14:35 -0500

    Added changes to data component

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-data',
@@ -13,36 +13,43 @@ export class DataComponent implements OnInit {
 
   usuario: Usuario = {
     nombrecompleto: {
-      nombre: 'Pedro',
-      apellido: 'Isaza'
+      nombre: 'test',
+      apellido: 'test'
     },
-    correo: 'pisaza@unal.edu.co'
+    correo: 'testing@unal.edu.co',
+    password1: '',
+    password2: '',
+    pasatiempos: ['']
+
   }
 
   constructor() {
 
     this.forma = new FormGroup({
       'nombrecompleto': new FormGroup({
-        'nombre': new FormControl('', [Validators.required,
-                                       Validators.minLength(3)]),
-        'apellido': new FormControl('', Validators.required)
+        'nombre': new FormControl('', [Validators.required,Validators.minLength(3)]),
+        'apellido': new FormControl('', [Validators.required, this.noVillegas])
       }),
-      'correo': new FormControl('', [Validators.required,
-      Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]
-      )
+      'correo': new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]),
+      'password1': new FormControl('', Validators.required),
+      'password2': new FormControl(''),
+      'pasatiempos': new FormArray([new FormControl('Correr', Validators.required)])
     });
+
+
+    this.forma.controls['password2'].setValidators(
+      [ Validators.required, this.noIgualPass2.bind(this.forma)]
+    );
+
 
     //Carga objeto por defecto.
     this.forma.setValue(this.usuario);
   }
 
   ngOnInit() {
-
-
   }
 
   guardarCambios() {
-
     console.log(this.forma.value);
     console.log(this.forma);
 
@@ -53,10 +60,37 @@ export class DataComponent implements OnInit {
       },
       correo: ""
     });
+
   }
 
-}
+  agregarPasatiempo() {
+    (<FormArray>this.forma.controls['pasatiempos']).push(
+      new FormControl('', Validators.required));
+  }
 
+
+  noVillegas(control: FormControl): { [s: string]: boolean } {
+    if (control.value === "mendoza") {
+      return {
+        novillegas: false
+      }
+    }
+    return null;
+  }
+
+
+  noIgualPass2(control: FormControl): { [s: string]: boolean } {
+
+    let forma: any = this;
+    if (control.value !== forma.controls['password1'].value) {
+
+      return {
+        noiguales: true
+      }
+    }
+    return null;
+  }
+}
 
 
 export interface Usuario {
@@ -65,4 +99,9 @@ export interface Usuario {
     nombre: string;
   }
   correo: string;
+  password1: string;
+  password2: string;
+  pasatiempos: string[]
+
+
 }
